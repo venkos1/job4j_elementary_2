53b646bd6472af2dc2846a2d95148560005a1c17
father <vvvenkos@gmail.com>
2024-12-05 19:12:03 +0300

    3.3. Фиксация изменений на github [#243564 #460611]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+            System.out.println("Агеев Константин");
+            System.out.println("10.01.2024");
+    }
+}
