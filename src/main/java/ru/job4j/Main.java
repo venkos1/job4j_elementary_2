33c9622004f1a7ee9fffa4b666ca56a1a6e3b0ae
father <vvvenkos@gmail.com>
2024-12-10 21:24:00 +0300

    3. Арифметические операции. [#185 #460806]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет мир!");
         System.out.println("Привет, друзья!");
+int two = 2;
+int six = 6;
+int four = 4;
+int five = 5;
+int sixDivTwo = six / two;
+int fiveMinusTwo = five - two;
+int fourTimeTwo = four * two;
+System.out.println("Результат деления 6 на 2: " + sixDivTwo);
+System.out.println("Разность 5 и 2: " + fiveMinusTwo);
+System.out.println("Произведение 4 и 2: " + fourTimeTwo);
     }
 }
